bc40b38ada977037bd718153f5819dbe993e60e6
Anton Bretting <sajfer@gmail.com>
2023-12-10 17:15:22 +0100

    fix: add support for setting volume through filters

--- encode.go
+++ encode.go
@@ -36,7 +36,7 @@ var (
 
 // EncodeOptions is a set of options for encoding dca
 type EncodeOptions struct {
-	Volume           int              // change audio volume (256=normal)
+	Volume           float32          // change audio volume (1.0=normal)
 	Channels         int              // audio channels
 	FrameRate        int              // audio sampling rate (ex 48000)
 	FrameDuration    int              // audio frame duration can be 20, 40, or 60 (ms)
@@ -65,8 +65,8 @@ func (e EncodeOptions) PCMFrameLen() int {
 
 // Validate returns an error if the options are not correct
 func (opts *EncodeOptions) Validate() error {
-	if opts.Volume < 0 || opts.Volume > 512 {
-		return errors.New("Out of bounds volume (0-512)")
+	if opts.Volume < 0 || opts.Volume > 2.0 {
+		return errors.New("Out of bounds volume (0-2.0)")
 	}
 
 	if opts.FrameDuration != 20 && opts.FrameDuration != 40 && opts.FrameDuration != 60 {
@@ -94,7 +94,7 @@ func (opts *EncodeOptions) Validate() error {
 
 // StdEncodeOptions is the standard options for encoding
 var StdEncodeOptions = &EncodeOptions{
-	Volume:           256,
+	Volume:           1.0,
 	Channels:         2,
 	FrameRate:        48000,
 	FrameDuration:    20,
@@ -212,6 +212,7 @@ func (e *EncodeSession) run() {
 		"-f", "ogg",
 		"-vbr", vbrStr,
 		"-compression_level", strconv.Itoa(e.options.CompressionLevel),
+		"-af", fmt.Sprintf("volume=%.2f", e.options.Volume),
 		"-ar", strconv.Itoa(e.options.FrameRate),
 		"-ac", strconv.Itoa(e.options.Channels),
 		"-b:a", strconv.Itoa(e.options.Bitrate * 1000),
